f4b0765818f275f6def1e04ac0d34643f5d3215d
DESKTOP-L9U2CF5\HARR <hpzhd.hp@gmail.com>
2022-10-29 10:59:43 +0530

    First Commit

---
+++ root/scripts/commands.js
@@ -0,0 +1,89 @@
+var youtube = "https://www.youtube.com/hpzhd/";
+var twitter = "https://twitter.com/hpzhd_hpt";
+var password = "hpzhdrocks";
+var linkedin = "https://www.linkedin.com/in/harshil-patel-027715211/";
+var instagram = "https://www.instagram.com/hpzhd/";
+var email = "mailto:harshil.hpz@gmail.com";
+
+whois = [
+  "<br>",
+  "Hey, I'm Harshil aka HP",
+  "I'm a web developer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about random content like games and memes.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a web developer for a local company",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "The paradox of â€œWho am I?â€ is: we never know, but, we constantly find out.",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' +
+    youtube +
+    '" target="_blank">youtube/hpzhd' +
+    "</a>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="_blank">twitter/hpzhd_hp' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/Harshil_Patel' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="_blank">instagram/hpzhd' +
+    "</a>",
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Harshil?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<a href="' +
+    youtube +
+    '" target="_blank">videos' +
+    "</a>         View YouTube videos",
+  "<br>",
+];
+
+banner = [
+  '<span class="index">HarshilPatel (HP) Not A Corporation. All Rights reserved.</span>',
+
+  "  █████   █████                             █████       ███  ████     ███████████             █████             ████ ",
+  "  ░███    ░███                             ░░███       ░░░  ░░███    ░░███░░░░░███           ░░███             ░░███ ",
+  "  ░███    ░███   ██████   ████████   █████  ░███████   ████  ░███     ░███    ░███  ██████   ███████    ██████  ░███ ",
+  "  ░███████████   ░░░░███ ░░███░░███ ███░░   ░███░░███ ░░███  ░███     ░██████████  ░░░░░███ ░░░███░    ███░░███ ░███ ",
+  "  ░███░░░░░███   ███████  ░███ ░░░ ░░█████  ░███ ░███  ░███  ░███     ░███░░░░░░    ███████   ░███    ░███████  ░███ ",
+  "  ░███    ░███  ███░░███  ░███      ░░░░███ ░███ ░███  ░███  ░███     ░███         ███░░███   ░███ ███░███░░░   ░███ ",
+  "  █████   █████ ░████████ █████     ██████  ████ █████ █████ █████    █████       ░░████████  ░░█████ ░░██████  █████",
+  "  ░░░░░   ░░░░░  ░░░░░░░░ ░░░░░     ░░░░░░  ░░░░ ░░░░░ ░░░░░ ░░░░░    ░░░░░         ░░░░░░░░    ░░░░░   ░░░░░░  ░░░░░ ",
+
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
